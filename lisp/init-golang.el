4121270cc1712f164be671fd6c446355dafe4b15
yahjiang <yahjiang@microsoft.com>
2018-03-08 13:29:49 +0800

    add

--- lisp/init-golang.el
+++ lisp/init-golang.el
@@ -27,6 +27,8 @@
                                         ; Godef jump key binding
   (local-set-key (kbd "M-.") 'godef-jump)
   (local-set-key (kbd "M-,") 'pop-tag-mark)
+  (local-set-key (kbd "M-/") 'comment-region)
+  (local-set-key (kbd "C-/") 'uncomment-region)
   )
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 
